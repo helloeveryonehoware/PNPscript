d8d85f05d7172aee5d85fe3d359f722608bc24a2
gemsvido <gemsvido@gmail.com>
2022-09-14 09:02:00 -0700

    FALLBACK IS FULLY OPERATIONAL!

--- src/main/kotlin/com/prodigypnp/conversion/Player.kt
+++ src/main/kotlin/com/prodigypnp/conversion/Player.kt
@@ -28,12 +28,24 @@ object Player {
 
     fun fallback (code : String) : String {
         val match = code.match("me.")
+
+        val beforeEqualP = code.replace(match, "_.player.").substringBefore("=");
+        val beforeEqualPD = code.replace(match, "_.player.data.").substringBefore("=");
+
+        val atPlayer = code.replace(match, "_.player.");
+        val atPlayerData = code.replace(match, "_.player.data.");
+
         return """
-            try {
-                ${code.replace(match, "_.player.")};
-            } catch {
-                ${code.replace(match, "_.player.data.")};
-            }
+            
+            if ($beforeEqualP) {
+                $atPlayer;
+            } else if (${beforeEqualPD}) {
+                $atPlayerData;
+            } else {
+                console.error("This didn't work.");
+            };
+
+            
         """.trimIndent().replace("    ", "").replace("\n", "");
     }
 
